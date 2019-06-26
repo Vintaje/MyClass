d148404f116d2afe23557f2a187e0d2d63871abd
Vintaje <vintaje@github.com>
2019-06-26 18:29:31 +0200

    indexar

--- index.php
+++ index.php
@@ -15,13 +15,46 @@ Adrian Sanchez
 
 
 ?>
-<p> hola </p>
 
-<p>18:16</p>
 
-18:17
 
-ultimo
 
-<p>Emilio</p>
 
+<?
+
+/**
+ * Indexar URL
+ */
+
+
+$url = parse_url($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+
+$ruta = $url['path'];
+
+$partes_ruta[0] = explode('/', $ruta);
+$partes_ruta = array_filter($partes_ruta);
+$partes_ruta = array_slice($partes_ruta,0);
+
+$ruta_elegida = 'vistas/404.php';
+
+
+if($partes_ruta[0] == 'www.myclass.es'){
+
+    if(count($partes_ruta) == 1){
+        $ruta_elegida = 'vistas/home.php';
+    } else if(count($partes_ruta) == 2){
+        switch($partes_ruta[1]){
+            case 'pagina':
+                $ruta_elegida = 'vistas/pagina.php';
+
+        }
+
+    }
+
+}else{
+    $ruta_elegida = 'vistas/404.php';
+
+}
+
+
+include_once $ruta_elegida
