886a3ea888b1c9751d1835e09ff08c16e58299b5
Vintaje <vintaje@github.com>
2019-06-27 13:36:39 +0200

    myClassDB

--- sql/bdd_principal.sql
+++ sql/bdd_principal.sql
@@ -1,86 +1,210 @@
-CREATE DATABASE MYCLASS;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-06-2019 a las 13:36:11
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.3.6
 
---MODIFICACION CARLOS
-USE MYCLASS;
-
-
--- NOTICIAS, RECOGE EL LINK DEL AGREGADOR RSS DE CADA PAGINA Y LO ENLACA A CADA FAMILIA PROFESIONAL
-CREATE TABLE NOTICIAS(
-	AGREGADOR VARCHAR(255) NOT NULL,
-	FAMILIA_PROF VARCHAR(255) NOT NULL
-);
-
--- USUARIOS, RELACION CON FAMILIA PROFESIONAL
-CREATE TABLE USUARIO(
-	ID INTEGER AUTO_INCREMENT UNIQUE,
-	CODIGO VARCHAR(255),
-	CORREO VARCHAR(255) NOT NULL UNIQUE,
-	AVATAR VARCHAR(255) NOT NULL UNIQUE DEFAULT 'www.myclass.es/img/avatardefault.jpg',
-	NOMBRE_FULL	VARCHAR(255) NOT NULL,
-	FAMILIA_PROF VARCHAR(255) NOT NULL,
-	FECHA_REG DATE NOT NULL,
-	EDAD INTEGER NOT NULL,
-	DNI VARCHAR(255),
-	ADMIN_VALUE TINYINT(1) NOT NULL DEFAULT '0',
-	
-	
-	PRIMARY KEY(CODIGO),
-	
-	CONSTRAINT FPROF_FK FOREIGN KEY FAMILIA_PROF REFERENCES NOTICIAS(FAMILIA_PROF)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- GRUPO, RELACION COD_OWNER CON CODIGO DE USUARIO
-CREATE TABLE GRUPO(
-	ID INTEGER AUTO_INCREMENT UNIQUE,
-	CODIGO VARCHAR(255),
-	NOMBRE VARCHAR(255) NOT NULL,
-	CAPACIDAD INTEGER NOT NULL DEFAULT '30', 
-	COD_OWNER VARCHAR(255) NOT NULL,
-	
-	PRIMARY KEY (CODIGO),
-	
-	
-	CONSTRAINT OWNER_FK FOREIGN KEY (COD_OWNER) REFERENCES USUARIO(CODIGO)
-		
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `myclass`
+--
 
--- RELACION ENTRE USUARIO - GRUPO, FOREIGN KEYS CON TABLA GRUPO Y TABLA USUARIO
-CREATE TABLE USERGROUP(
-	COD_USER VARCHAR(255) NOT NULL,
-	COD_GROUP VARCHAR(255) NOT NULL,
-	
-	
-	CONSTRAINT USER_FK FOREIGN KEY (COD_USER) REFERENCES USUARIO(CODIGO),
-	CONSTRAINT GROUP_FK FOREIGN KEY (COD_GROUP) REFERENCES GRUPO(CODIGO)
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `grupo`
+--
 
+CREATE TABLE `grupo` (
+  `ID` int(10) NOT NULL,
+  `CODIGO` varchar(255) NOT NULL,
+  `NOMBRE` varchar(255) NOT NULL,
+  `CAPACIDAD` int(10) NOT NULL DEFAULT 30,
+  `COD_OWNER` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
--- CREACION DE TAREA QUE ENLAZA CADA UNA DE ESTAS A UN GRUPO
-CREATE TABLE TAREAS(
-	CODIGO VARCHAR(255),
-	COD_GRUPO VARCHAR(255) NOT NULL,
-	TITLE_TAREA VARCHAR(255) NOT NULL,
-	BODY_TAREA VARCHAR(255) NOT NULL,
-	FECHA_ENTREGA DATE NOT NULL,
-	
-	
-	PRIMARY KEY (CODIGO)
-	
-);
+--
+-- Estructura de tabla para la tabla `noticias`
+--
 
+CREATE TABLE `noticias` (
+  `AGREGADOR` varchar(255) NOT NULL,
+  `FAMILIA_PROF` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- NOTAS DE CADA USUARIOS ENLAZADAS A UNA TAREA Y A UN USUARIO CON LAS FKs
-CREATE TABLE USERNOTAS(
-	COD_USER VARCHAR(255) NOT NULL,
-	COD_TAREA VARCHAR(255) NOT NULL,
-	NOTA FLOAT NOT NULL,
-	
-	
-	CONSTRAINT USER_FK FOREIGN KEY (COD_USER) REFERENCES USUARIO(CODIGO),
-	CONSTRAINT WORK_FK FOREIGN KEY (COD_TAREA) REFERENCES TAREAS(CODIGO)
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `CODIGO` varchar(255) NOT NULL,
+  `COD_GRUPO` varchar(255) NOT NULL,
+  `TITLE_TAREA` varchar(255) NOT NULL,
+  `BODY_TAREA` varchar(255) NOT NULL,
+  `FECHA_ENTREGA` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usergroup`
+--
+
+CREATE TABLE `usergroup` (
+  `COD_USER` varchar(255) NOT NULL,
+  `COD_GROUP` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usernotas`
+--
+
+CREATE TABLE `usernotas` (
+  `COD_USER` varchar(255) NOT NULL,
+  `COD_TAREA` varchar(255) NOT NULL,
+  `NOTA` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID` int(10) NOT NULL,
+  `CODIGO` varchar(255) NOT NULL,
+  `CORREO` varchar(255) NOT NULL,
+  `AVATAR` varchar(255) NOT NULL DEFAULT 'www.myclass.es/img/avatardefault.jpg',
+  `NOMBRE_FULL` varchar(255) NOT NULL,
+  `FAMILIA_PROF` varchar(255) NOT NULL,
+  `FECHA_REG` date NOT NULL,
+  `EDAD` int(10) NOT NULL,
+  `DNI` varchar(255) DEFAULT NULL,
+  `ADMIN_VALUE` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD PRIMARY KEY (`CODIGO`),
+  ADD UNIQUE KEY `ID` (`ID`),
+  ADD KEY `user_group_fk` (`COD_OWNER`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`FAMILIA_PROF`);
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`CODIGO`),
+  ADD KEY `task_group_fk` (`COD_GRUPO`);
+
+--
+-- Indices de la tabla `usergroup`
+--
+ALTER TABLE `usergroup`
+  ADD KEY `user_cod_fk` (`COD_USER`),
+  ADD KEY `group_cod_fk` (`COD_GROUP`);
+
+--
+-- Indices de la tabla `usernotas`
+--
+ALTER TABLE `usernotas`
+  ADD KEY `user_notas_fk` (`COD_USER`),
+  ADD KEY `task_notas_fk` (`COD_TAREA`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`CODIGO`),
+  ADD UNIQUE KEY `ID` (`ID`),
+  ADD UNIQUE KEY `CORREO` (`CORREO`),
+  ADD UNIQUE KEY `AVATAR` (`AVATAR`),
+  ADD KEY `user_prof_fk` (`FAMILIA_PROF`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD CONSTRAINT `user_group_fk` FOREIGN KEY (`COD_OWNER`) REFERENCES `usuario` (`CODIGO`);
+
+--
+-- Filtros para la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD CONSTRAINT `task_group_fk` FOREIGN KEY (`COD_GRUPO`) REFERENCES `grupo` (`CODIGO`);
+
+--
+-- Filtros para la tabla `usergroup`
+--
+ALTER TABLE `usergroup`
+  ADD CONSTRAINT `group_cod_fk` FOREIGN KEY (`COD_GROUP`) REFERENCES `grupo` (`CODIGO`),
+  ADD CONSTRAINT `user_cod_fk` FOREIGN KEY (`COD_USER`) REFERENCES `usuario` (`CODIGO`);
+
+--
+-- Filtros para la tabla `usernotas`
+--
+ALTER TABLE `usernotas`
+  ADD CONSTRAINT `task_notas_fk` FOREIGN KEY (`COD_TAREA`) REFERENCES `tareas` (`CODIGO`),
+  ADD CONSTRAINT `user_notas_fk` FOREIGN KEY (`COD_USER`) REFERENCES `usuario` (`CODIGO`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `user_prof_fk` FOREIGN KEY (`FAMILIA_PROF`) REFERENCES `noticias` (`FAMILIA_PROF`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
