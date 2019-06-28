a86a3f821adea216a4f45475c874847520478af3
Vintaje <vintaje@github.com>
2019-06-28 10:59:57 +0200

    conexion

--- app/conexion.inc.php
+++ app/conexion.inc.php
@@ -2,17 +2,17 @@
 include_once 'config.inc.php';
 
 class conexion {
-    
+    private static $conexion;
+
     function conectarBD(){
+        if(!isset($conexion)){
        return $conexion=mysqli_connect(NOMBRE_SERVIDOR,NOMBRE_USUARIO,PASSWORD,NOMBRE_DB);
+        }
      }
     
-     function desconectarBD(){
+    function desconectarBD(){
          if ($conexion!=null){
             $conexion=null;
          }
     }
 }
-
-
-?>
