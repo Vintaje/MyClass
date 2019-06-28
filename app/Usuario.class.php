b884b375fbc98678b6854a87c6543eec3d06de88
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2019-06-28 11:43:05 +0200

    usuario

--- app/Usuario.class.php
+++ app/Usuario.class.php
@@ -1,5 +1,6 @@
 <?php
-class Usuario {
+class Usuario
+{
     private $codigo;
     private $password;
     private $correo;
@@ -11,50 +12,105 @@ class Usuario {
     private $dni;
     private $avatar;
 
-    public function __construct($correo, $nombre_completo, $codigo, $password, $sexo, $familia_prof, $fecha_reg,$edad,$dni,$avatar){
-        $this -> correo = $correo;
-        $this -> nombre_completo = $nombre_completo;
-        $this -> codigo = $codigo;
-        $this -> password = $password;
-        $this -> familia_prof = $familia_prof;
-        $this -> edad = $edad;
-        $this -> dni = $dni;
-        $this -> avatar = $avatar;
-        $this -> sexo = $sexo;
-        $this -> fecha_reg = $fecha_reg;
+    public function __construct($correo, $nombre_completo, $codigo, $password, $sexo, $familia_prof, $fecha_reg, $edad, $dni, $avatar)
+    {
+        $this->correo = $correo;
+        $this->nombre_completo = $nombre_completo;
+        $this->codigo = $codigo;
+        $this->password = $password;
+        $this->familia_prof = $familia_prof;
+        $this->edad = $edad;
+        $this->dni = $dni;
+        $this->avatar = $avatar;
+        $this->sexo = $sexo;
+        $this->fecha_reg = $fecha_reg;
     }
 
-    public function getCodigo(){
-        return $this -> codigo;
+    public function getCodigo()
+    {
+        return $this->codigo;
     }
-    public function getPassword(){
-        return $this -> password;
+    public function getPassword()
+    {
+        return $this->password;
     }
-    public function getCorreo(){
-        return $this -> correo;
+    public function getCorreo()
+    {
+        return $this->correo;
     }
-    public function getNombreCompleto(){
-        return $this -> nombre_completo;
+    public function getNombreCompleto()
+    {
+        return $this->nombre_completo;
     }
-    public function getSexo(){
-        return $this -> sexo;
+    public function getSexo()
+    {
+        return $this->sexo;
     }
-    public function getFamiliaProf(){
-        return $this -> familia_prof;
+    public function getFamiliaProf()
+    {
+        return $this->familia_prof;
     }
-    public function getFechaReg(){
-        return $this -> fecha_reg;
+    public function getFechaReg()
+    {
+        return $this->fecha_reg;
     }
-    public function getEdad(){
-        return $this -> edad;
+    public function getEdad()
+    {
+        return $this->edad;
     }
-    public function getDni(){
-        return $this -> dni;
+    public function getDni()
+    {
+        return $this->dni;
     }
-    public function getAvatar(){
-        return $this -> avatar;
+    public function getAvatar()
+    {
+        return $this->avatar;
     }
+
+
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+    }
+    public function setNombreCompleto($nombre_completo)
+    {
+        $this->nombre_completo = $nombre_completo;
+    }
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+    }
+    public function setFamiliaProf($familia_prof)
+    {
+        $this->familia_prof = $familia_prof;
+    }
+    public function setFechaReg($fecha_reg)
+    {
+        $this->fecha_reg = $fecha_reg;
+    }
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+    }
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+    }
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+    }
+
+
     
-}
 
-?>
+
+
+
+
+}
