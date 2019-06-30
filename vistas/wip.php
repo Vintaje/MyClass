912f3431717fe2c201eec70169c407d70b09e86e
Vintaje <vintaje@github.com>
2019-06-30 13:31:44 +0200

    -+41526

---
+++ vistas/wip.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>myClass</title>
+    </head>
+    <body>
+        <center>
+        <h1>Estamos creando la web, diculpen las molestias.</h1>
+
+        <img src="img/obras.gif">
+        
+        </center>
+    </body>
+</html>
