92b9473331b31511c7d86bd362cd343dc29757b8
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2019-07-01 13:46:01 +0200

    upduser

--- app/RepositorioUsuario.inc.php
+++ app/RepositorioUsuario.inc.php
@@ -81,4 +81,68 @@ class RepositorioUsuario
 
         return $usuario;
     }
+
+    public static function updUsuario($conexion, $usuario, $modo)
+    {
+        $usuario_actualizado = false;
+
+        if (isset($conexion)) {
+            try {
+
+                switch ($modo) {
+                    case 1: //modo 1 actualizar nombre de usuario.
+
+                        $sql = "update usuario set NOMBRE_FULL = :nombre_full where codigo = :codigo";
+                        break;
+                    case 2: //modo 2 actualizar passwd de usuario.
+
+                        $sql = "update usuario set passwd = :passwd where codigo = :codigo";
+                        break;
+                    case 3: //modo 3 actualizar avatar de usuario.
+                        $sql = "update usuario set avatar = :avatar where codigo = :codigo";
+                        break;
+                    case 4: //modo 4 actualizar sexo de usuario.
+                        $sql = "update usuario set sexo = :sexo where codigo = :codigo";
+                        break;
+                        case 5: //modo 5 actualizar correo de usuario.
+                        $sql = "update usuario set correo = :correo where codigo = :codigo";
+                        break;
+                        case 6: //modo 6 actualizar familia_prof de usuario.
+                        $sql = "update usuario set familia_prof = :familia_prof where codigo = :codigo";
+                        break;
+                }
+
+
+
+
+
+                $codigotemp = $usuario->GetCodigo();
+                $correotemp = $usuario->GetCorreo();
+                $passwdtemp = $usuario->getPassword();
+                $avatartemp = $usuario->GetAvatar();
+                $nombretemp = $usuario->GetNombreCompleto();
+                $familia_proftemp = $usuario->getFamiliaProf();
+                $edadtemp = $usuario->getEdad();
+                $dnitemp = $usuario->getDni();
+                $sexotemp = $usuario->getSexo();
+
+                $sentencia = $conexion->prepare($sql);
+                $sentencia->bindParam(':codigo', $codigotemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':correo', $correotemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':passwd', $passwdtemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':avatar', $avatartemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':nombre_full', $nombretemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':familia_prof', $familia_proftemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':edad', $edadtemp, PDO::PARAM_INT);
+                $sentencia->bindParam(':dni', $dnitemp, PDO::PARAM_STR);
+                $sentencia->bindParam(':sexo', $sexotemp, PDO::PARAM_STR_CHAR);
+
+                $usuario_actualizado = $sentencia->execute();
+            } catch (PDOException $ex) {
+                print 'ERROR' . $ex->getMessage();
+            }
+        }
+
+        return $usuario_actualizado;
+    }
 }
