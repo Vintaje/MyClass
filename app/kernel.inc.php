afa2002ca1af65ebad5d3ac877edb02261e67192
Vintaje <vintaje@github.com>
2019-07-02 11:28:16 +0200

    kernel_encriptacion

--- app/kernel.inc.php
+++ app/kernel.inc.php
@@ -3,10 +3,28 @@
 class kernel
 {
     //-------------------------------------------------------------------------------------- Emilio
-    public static function encrypt($param)
-    {
-        $hash = password_hash($param, PASSWORD_DEFAULT);
 
-        return $hash;
+    //Si el action llega con 'encrypt' , encriptara nuestro mensaje, 'decrypt' por ende, desencriptara
+    function encrypt_decrypt($action, $string) {
+
+
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'MyClass2019++@K';
+        $secret_iv = 'IVMyClass++2019@';
+        // hash
+        $key = hash('sha256', $secret_key);
+        
+        // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+        if ( $action == 'encrypt' ) {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        } else if( $action == 'decrypt' ) {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+        return $output;
     }
+
+
 }
