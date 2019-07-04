ae25d7f3e0ebf2234802a30d981cfd1aeda0e559
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-07-04 14:21:21 +0200

    commitCss

--- vistas/home.php
+++ vistas/home.php
@@ -51,19 +51,15 @@
 
 <!-- Footer -->
 <footer class="page-footer font-small stylish-color-dark pt-4">
-
     <!-- Footer Links -->
     <div class="container text-center text-md-left">
-
         <!-- Grid row -->
         <div class="row">
-
             <!-- Grid column -->
             <div class="col-md-12 mx-auto">
-
                 <!-- Content -->
                 <h5><i class="far fa-question-circle"></i> About us</h5>
-                <p>En myClass tenemos un objetivo claro: Ofrecer a todos los estudiantes, en cualquier nivel
+                <p class="desc">En myClass tenemos un objetivo claro: Ofrecer a todos los estudiantes, en cualquier nivel
                     de estudios, una herramienta para compartir sus conocimientos. 
                     Pero no solo eso. En myClass, nos preocupamos de tu presente (ofreciéndote noticias en función de tu
                     familia profesional) y de tu futuro (conectándote con una red de empresas que buscan tu talento). 
@@ -78,29 +74,29 @@
     <hr>
     <hr>
     <!-- Botones para las redes sociales -->
-    <ul class="list-unstyled list-inline text-center">
+    <ul class="list-unstyled list-inline text-center" id="botones">
         <li class="list-inline-item">
-            <a class="btn-floating btn-fb mx-1">
+            <a class="btn-floating btn-fb mx-1" id="fb">
                 <i class="fa fa-facebook"> </i>
             </a>
         </li>
         <li class="list-inline-item">
-            <a class="btn-floating btn-tw mx-1">
+            <a class="btn-floating btn-tw mx-1" id="tw">
                 <i class="fa fa-twitter"> </i>
             </a>
         </li>
         <li class="list-inline-item">
-            <a class="btn-floating btn-yt mx-1">
+            <a class="btn-floating btn-yt mx-1" id="yt">
                 <i class="fa fa-youtube"> </i>
             </a>
         </li>
         <li class="list-inline-item">
-            <a class="btn-floating btn-li mx-1">
+            <a class="btn-floating btn-li mx-1" id="lnk">
                 <i class="fa fa-linkedin"> </i>
             </a>
         </li>
         <li class="list-inline-item">
-            <a class="btn-floating btn-dribbble mx-1">
+            <a class="btn-floating btn-dribbble mx-1" id ="ig">
                 <i class="fa fa-instagram"> </i>
             </a>
         </li>
