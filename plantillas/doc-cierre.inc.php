1371cc2f09edca1ca1afc79fc47357e62a9b609e
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-07-04 16:07:37 +0200

    commitFooter

--- plantillas/doc-cierre.inc.php
+++ plantillas/doc-cierre.inc.php
@@ -1,12 +1,6 @@
-
 <?php
 conexion::desconectarBD();
 ?>
-
-<br>
-<div class="footer">
-    <p class="text-center rights" style="color: #fff;">MyClass ©</p>
-</div>
 
 
 <script src="js/bootstrap.js"></script>
--- vistas/home.php
+++ vistas/home.php
@@ -8,8 +8,6 @@
     <div class="col-md-1"></div>
 </div>
 
-
-
 <div class="row">
     <div class="col-md-2"></div>
     <div class="col-md-4">
@@ -49,58 +47,55 @@
     </div>
 </div>
 
-<!-- Footer -->
-<footer class="page-footer font-small stylish-color-dark pt-4">
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left">
-        <!-- Grid row -->
-        <div class="row">
-            <!-- Grid column -->
-            <div class="col-md-12 mx-auto">
-                <!-- Content -->
-                <h5><i class="far fa-question-circle"></i> About us</h5>
-                <p class="desc">En myClass tenemos un objetivo claro: Ofrecer a todos los estudiantes, en cualquier nivel
-                    de estudios, una herramienta para compartir sus conocimientos. 
-                    Pero no solo eso. En myClass, nos preocupamos de tu presente (ofreciéndote noticias en función de tu
-                    familia profesional) y de tu futuro (conectándote con una red de empresas que buscan tu talento). 
+<div class="container">
+    <footer class="page-footer font-small stylish-color-dark pt-4">
+        <div class="container text-center text-md-left">
+    
+            <div class="row">
+           
+                <div class="col-md-12 mx-auto">
+                   
+                    <h5><i class="far fa-question-circle"></i> About us</h5>
+                    <p class="desc">En myClass tenemos un objetivo claro: Ofrecer a todos los estudiantes, en cualquier nivel
+                        de estudios, una herramienta para compartir sus conocimientos.
+                        Pero no solo eso. En myClass, nos preocupamos de tu presente (ofreciéndote noticias en función de tu
+                        familia profesional) y de tu futuro (conectándote con una red de empresas que buscan tu talento).
 
-                    Así que, ¿qué esperas? ¡Entra en la clase del futuro! <br/>
-
-                    #myClassCeos 
-                </p>
+                        Así que, ¿qué esperas? ¡Entra en la clase del futuro! <br />
+                        #myClassCeos
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-    <hr>
-    <hr>
-    <!-- Botones para las redes sociales -->
-    <ul class="list-unstyled list-inline text-center" id="botones">
-        <li class="list-inline-item">
-            <a class="btn-floating btn-fb mx-1" id="fb">
-                <i class="fa fa-facebook"> </i>
-            </a>
-        </li>
-        <li class="list-inline-item">
-            <a class="btn-floating btn-tw mx-1" id="tw">
-                <i class="fa fa-twitter"> </i>
-            </a>
-        </li>
-        <li class="list-inline-item">
-            <a class="btn-floating btn-yt mx-1" id="yt">
-                <i class="fa fa-youtube"> </i>
-            </a>
-        </li>
-        <li class="list-inline-item">
-            <a class="btn-floating btn-li mx-1" id="lnk">
-                <i class="fa fa-linkedin"> </i>
-            </a>
-        </li>
-        <li class="list-inline-item">
-            <a class="btn-floating btn-dribbble mx-1" id ="ig">
-                <i class="fa fa-instagram"> </i>
-            </a>
-        </li>
-    </ul>
-    <div class="footer-copyright text-center py-3">© 2019 Copyright: myClass
-    </div>
-</footer>
+        <!-- Botones para las redes sociales -->
+        <ul class="list-unstyled list-inline text-center" id="botones">
+            <li class="list-inline-item">
+                <a class="btn-floating btn-fb mx-1" id="fb">
+                    <i class="fa fa-facebook"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="https://twitter.com/MyClassCEOs" class="btn-floating btn-tw mx-1" id="tw">
+                    <i class="fa fa-twitter"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-yt mx-1" id="yt">
+                    <i class="fa fa-youtube"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-li mx-1" id="lnk">
+                    <i class="fa fa-linkedin"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-dribbble mx-1" id="ig">
+                    <i class="fa fa-instagram"> </i>
+                </a>
+            </li>
+        </ul>
+        <div class="footer-copyright text-center py-3">© 2019 Copyright: myClass
+        </div>
+    </footer>
+</div>
