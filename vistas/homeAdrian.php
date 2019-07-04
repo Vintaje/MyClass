63a04114b3a4c7e9ccde9f66371f25409fcec235
Adrián <adrianscc11@gmail.com>
2019-07-04 13:00:21 +0200

    homeAdrian

---
+++ vistas/homeAdrian.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/bootstrap-reboot.min.css" type="text/css"/>
+        <link rel="stylesheet" href="css/styleHome.css" type="text/css">
+
+
+    </head>
+    <body>
+
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+
+        <div class="main">
+
+
+            <div class="container">
+                <center>
+                    <div class="middle">
+                        <div id="login">
+
+                            <form action="javascript:void(0);" method="get">
+
+                                <fieldset class="clearfix">
+
+                                    <p ><span class="fa fa-user"></span><input type="text"  Placeholder="Username" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
+                                    <p><span class="fa fa-lock"></span><input type="password"  Placeholder="Password" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
+
+                                    <div>
+                                        <span style="width:48%; text-align:left;  display: inline-block;"><a class="small-text" href="#">Forgot
+                                                password?</a></span>
+                                        <span style="width:50%; text-align:right;  display: inline-block;"><input type="submit" value="Sign In"></span>
+                                    </div>
+
+                                </fieldset>
+                                <div class="clearfix"></div>
+                            </form>
+
+                            <div class="clearfix"></div>
+
+                        </div> <!-- end login -->
+                        <div class="logo">
+
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1200px-Adidas_Logo.svg.png" class="marca">
+
+                            <div class="clearfix"></div>
+                        </div>
+
+                    </div>
+                </center>
+            </div>
+
+        </div>
+
+    </body>
+</html>
