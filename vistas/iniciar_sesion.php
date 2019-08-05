d68df9d1512c522e051a12ae100685d18eddf81d
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2019-08-05 11:49:36 +0200

    inicio

--- vistas/iniciar_sesion.php
+++ vistas/iniciar_sesion.php
@@ -7,11 +7,11 @@
             <form>
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    <input type="email" class="form-control" id="email" placeholder="Introduce tu email" required>
                 </div>
                 <div class="form-group">
                     <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    <input type="password" class="form-control" id="pwd" placeholder="Introduce tu password" required>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success btn-lg">Submit</button>
