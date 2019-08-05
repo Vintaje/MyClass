09c98d65334288281fa6b294115ee7b5ccc347be
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-08-05 12:00:42 +0200

    required  registro

--- vistas/registro.php
+++ vistas/registro.php
@@ -3,22 +3,22 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="inputEmail4">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
+                <input type="email" class="form-control" id="inputEmail4" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputPassword4">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
+                <input type="password" class="form-control" id="inputPassword4" required>
             </div>
         </div>
         <div class="form-group">
             <label for="inputAddress">Nombre completo</label>
-            <input type="text" class="form-control" id="inputAddress">
+            <input type="text" class="form-control" id="inputAddress" required>
         </div>
 
         <div class="form-row align-items-center">
             <div class="col-auto my-1">
                 <label class="mr-sm-2" for="inlineFormCustomSelect">Familia Profesional</label>
-                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" required>
                     <option disabled selected value>Familias</option>
                     <option value="1"> Informática</option>
                     <option value="2">Comercio</option>
@@ -29,7 +29,7 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="inputCity">Edad</label>
-                <input type="text" class="form-control" id="inputCity">
+                <input type="text" class="form-control" id="inputCity" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="inputState">State</label>
@@ -43,14 +43,14 @@
         </div>
         <div class="form-group">
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <input class="form-check-input" type="checkbox" id="gridCheck" required>
                 <label class="form-check-label" for="gridCheck">
                     Acepto las condiciones de uso y que myClass me folle el ano
                 </label>
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
 
 
