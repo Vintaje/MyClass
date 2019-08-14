15df282b7ff2f33c167a20646db95dec842bf615
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-08-14 17:27:38 +0200

    colores

--- vistas/principal.php
+++ vistas/principal.php
@@ -18,14 +18,8 @@
 
 <div class="container p0">
 
-<<<<<<< HEAD
     <nav class="navbar navbar-expand-lg navbar-light" id="menu" style="background-color: #e3f2fd;"> <a class="navbar-brand" href="#">myClass</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-=======
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="menu"> <a class="navbar-brand" href="#">myClass</a>
-        <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">     <div class="animated-icon1"><span></span><span></span><span></span></div>
-</button>
->>>>>>> c43bd68ae57a6c7ae63cb80e9c2c66c911e41dce
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active"> <a class="nav-link" href="#"> Inicio <span class="sr-only">(current)</span></a> </li>
