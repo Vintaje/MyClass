c43bd68ae57a6c7ae63cb80e9c2c66c911e41dce
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2019-08-14 15:16:39 +0200

    boton menu

--- vistas/principal.php
+++ vistas/principal.php
@@ -157,14 +157,7 @@ $('.first-button').on('click', function () {
 
   $('.animated-icon1').toggleClass('open');
 });
-$('.second-button').on('click', function () {
 
-  $('.animated-icon2').toggleClass('open');
-});
-$('.third-button').on('click', function () {
-
-  $('.animated-icon3').toggleClass('open');
-});
 });
 
 
