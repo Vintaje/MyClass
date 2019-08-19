f594761a56439e479eb75387c4b2be6724070dd6
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-08-19 21:35:45 +0200

    clases desplegables

--- vistas/principal.php
+++ vistas/principal.php
@@ -1,13 +1,13 @@
 <?php
 
-if(isset($_SESSION['name_user'])){
+if (isset($_SESSION['name_user'])) {
     $usuario = $_SESSION['name_user'];
-?>
+    ?>
 <?php
-if(strpos($_SERVER['REQUEST_URI'],'.php')){
-    header('Location: ../');
-}
-?>
+    if (strpos($_SERVER['REQUEST_URI'], '.php')) {
+        header('Location: ../');
+    }
+    ?>
 
 
 <!--Menú de navegación-->
@@ -26,8 +26,12 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
         }
     }
 
-    .principal{
-        margin-top: 70px; 
+    .principal {
+        margin-top: 70px;
+    }
+
+    .boton {
+        margin-bottom: 10px;
     }
 </style>
 
@@ -35,8 +39,9 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
 <div class="container p0">
 
     <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light" id="menu"> <a class="navbar-brand" href="#">myClass</a>
-        <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">     <div class="animated-icon1"><span></span><span></span><span></span></div>
-</button>
+        <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="animated-icon1"><span></span><span></span><span></span></div>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active"> <a class="nav-link" href="#"> Inicio <span class="sr-only">(current)</span></a> </li>
@@ -88,7 +93,7 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
                             <div class="header">
                                 <span class="left button" id="prev"> &lang; </span>
                                 <span class="left hook"></span>
-                                <span class="month-year" id="label">  </span>
+                                <span class="month-year" id="label"> </span>
                                 <span class="right hook"></span>
                                 <span class="right button" id="next"> &rang; </span>
                             </div>
@@ -106,19 +111,19 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
                                 <table class="curr">
                                     <tbody>
                                         <tr>
-                                           
+
                                         </tr>
                                         <tr>
-                                            
+
                                         </tr>
                                         <tr>
-                                            
+
                                         </tr>
                                         <tr>
-                                            
+
                                         </tr>
                                         <tr>
-                                            
+
                                         </tr>
                                     </tbody>
                                 </table>
@@ -138,8 +143,55 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
                     <div class="card mb-3">
                         <img src="https://www.ealde.es/wp-content/uploads/2017/03/brainstorming.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title"><a href="mis-clases">Mis clases</a></h5>
+                            <h5 class="card-title"></h5>
                             <p class="card-text">En este apartado podrás ver las clases a las que perteneces</p>
+                            <div class="card-body d-flex">
+                                <button class="btn btn-primary ml-auto boton" type="button" data-toggle="collapse" data-target="#clases" aria-expanded="false" aria-controls="clases">
+                                    Ver
+                                </button>
+                            </div>
+                            <!--Parte que se oculta -->
+                            <div class="collapse" id="clases">
+                                <div class="card card-body">
+                                    <div id="accordion">
+                                        <div class="card">
+                                            <div class="card-header" id="headingTwo">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                        Bases de Datos
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Clase de primero de BBDD
+                                                    <div class="card-body d-flex">
+                                                        <a class="btn btn-primary ml-auto" href="mis-clases" role="button">Ir</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <div class="card-header" id="headingThree">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                        Programación
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Clase de primero de Programación
+                                                    <div class="card-body d-flex">
+                                                        <a class="btn btn-primary ml-auto" href="mis-clases" role="button">Ir</a>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -166,15 +218,14 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
 <!--JavaScript para el calendario -->
 
 <script>
+    $(document).ready(function() {
 
-$(document).ready(function () {
+        $('.first-button').on('click', function() {
 
-$('.first-button').on('click', function () {
+            $('.animated-icon1').toggleClass('open');
+        });
 
-  $('.animated-icon1').toggleClass('open');
-});
-
-});
+    });
 
 
 
@@ -322,7 +373,7 @@ $('.first-button').on('click', function () {
 </script>
 
 <?php
-}else{
+} else {
 
     header('Location: http://localhost/myclass');
 }
