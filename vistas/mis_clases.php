fd7dc789576c07d28b9a7eb266daf7f90668f1ee
Adrián <adrianscc11@gmail.com>
2019-08-21 12:45:27 +0200

    misclases

--- vistas/mis_clases.php
+++ vistas/mis_clases.php
@@ -32,127 +32,96 @@ if (isset($_SESSION['name_user'])) {
 </div>
 -->
 <style>
-    .menu_vertical {
-        max-width: 15%;
-        min-height: 100%;
-        float: left;
+    .container-fluid {
+        padding-left: 0px;
+
     }
-    #contenido{
-        float: right;
+
+    a {
+        color: white;
+    }
+
+    li :hover {
+        color: thistle;
     }
 </style>
 
-<body>
-    <div class="menu_vertical">
-        <nav class="navbar navbar bg-light bg-dark navbar-dark ">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"> <a class="nav-link" href="#"> Inicio <span class="sr-only">(current)</span></a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Agregar amigo</a> </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Noticias
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown"> <a class="dropdown-item" href="#">General</a> <a class="dropdown-item" href="#">Mi familia profesional</a>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-2 collapse d-md-flex bg-secondary pt-2 min-vh-100" id="sidebar">
+            <ul class="nav flex-column flex-nowrap">
+                <li class="nav-item active"> <a class="nav-link" href="panel-usuario"> Inicio <span class="sr-only">(current)</span></a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#"> Agregar amigo</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="NuevaClase"> Crear clase</a> </li>
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#submenu1" data-toggle="collapse" data-target="#submenu1">Noticias</a>
+                    <div class="collapse" id="submenu1" aria-expanded="false">
+                        <ul class="flex-column pl-2 nav">
+                            <li class="nav-item"><a class="nav-link py-0" href="#">General</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1">Mi familia profesional</a>
+                                <div class="collapse" id="submenu1sub1" aria-expanded="false">
+                                    <ul class="flex-column nav pl-4">
+                                        <li class="nav-item">
+                                            <a class="nav-link p-1" href="#">
+                                                <i class="fa fa-fw fa-clock-o"></i> Daily
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link p-1" href="#">
+                                                <i class="fa fa-fw fa-dashboard"></i> Dashboard
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link p-1" href="#">
+                                                <i class="fa fa-fw fa-bar-chart"></i> Charts
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link p-1" href="#">
+                                                <i class="fa fa-fw fa-compass"></i> Areas
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </li>
+
             </ul>
-            <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a class="nav-link" href="logout">Cerrar sesión</a></li>
-            </ul>
-        </nav>
-    </div>
+        </div>
 
+        <div class="col pt-2">
 
-
-
-    <div class="container p0 principal" id="contenido">
-        <div class="row p0">
-            
-            <!-- Parte central de la página -->
-            <div class="col-md-8 p0" id="centro">
-
-                <div class="row p0">
-                    <div class="col-md-12 p0">
-                        <div class="card mb-3">
-                            <img src="https://www.ealde.es/wp-content/uploads/2017/03/brainstorming.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title"><i class="fas fa-graduation-cap"></i> Mis Clases</h5>
-                                <p class="card-text">En este apartado podrás ver las clases a las que perteneces</p>
-                                <div class="card-body d-flex">
-                                    <button class="btn btn-primary ml-auto boton" type="button" data-toggle="collapse" data-target="#clases" aria-expanded="false" aria-controls="clases">
-                                        Ver
-                                    </button>
-                                </div>
-                                <!--Parte que se oculta -->
-                                <div class="collapse" id="clases">
-                                    <div class="card card-body acordeon">
-                                        <div id="accordion">
-                                            <div class="card">
-                                                <div class="card-header" id="headingTwo">
-                                                    <h5 class="mb-0">
-                                                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                            Bases de Datos
-                                                        </button>
-                                                    </h5>
-                                                </div>
-                                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                                                    <div class="card-body">
-                                                        Clase de primero de BBDD
-                                                        <div class="card-body d-flex">
-                                                            <a class="btn btn-primary ml-auto" href="mis-clases" role="button">Ir</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="card">
-                                                <div class="card-header" id="headingThree">
-                                                    <h5 class="mb-0">
-                                                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                            Programación
-                                                        </button>
-                                                    </h5>
-                                                </div>
-                                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                                                    <div class="card-body">
-                                                        Clase de primero de Programación
-                                                        <div class="card-body d-flex">
-                                                            <a class="btn btn-primary ml-auto" href="mis-clases" role="button">Ir</a>
-                                                        </div>
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+            <div class="col">
+                <div class="card border-secondary mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Próximos eventos</div>
+                    <div class="card-body text-secondary">
+                        <h5 class="card-title">Trabajos y Examenes</h5>
                     </div>
                 </div>
-
-                <div class="row p0">
-                    <div class="col-md-12 p0">
-                        <div class="card mb-3">
-                            <img src="https://www.ealde.es/wp-content/uploads/2017/03/comunicacion-interna.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title"><i class="fas fa-search"></i> Explorar</h5>
-                                <p class="card-text">Encuentra las clases perfectas para ti</p>
-                            </div>
-                        </div>
+            </div>
+            
+        </div>
+        <div class="col pt-3">
+            <div class="col">
+                <div class="card border-secondary mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Chat</div>
+                    <div class="card-body text-secondary">
+                        <h5 class="card-title">Chat de Clase</h5>
 
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 
-</body>
 
-
-
-
-
-
+</div>
+</div>
+</div>
 
 
 
