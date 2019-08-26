c2fa71a8e6e90698a626637cca561c837372a314
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2019-08-26 17:57:51 +0200

    Menú enlazado en todas las vistas

--- vistas/crearClase.php
+++ vistas/crearClase.php
@@ -39,7 +39,7 @@ if(strpos($_SERVER['REQUEST_URI'],'.php')){
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active"> <a class="nav-link" href="panel-usuario"> Inicio <span class="sr-only">(current)</span></a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#"> Agregar amigo</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="AgregarAmigo"> Agregar amigo</a> </li>
                 <li class="nav-item"> <a class="nav-link" href="NuevaClase"> Crear clase</a> </li>
                 <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Noticias
                     </a>
--- vistas/mis_clases.php
+++ vistas/mis_clases.php
@@ -57,7 +57,7 @@ if (isset($_SESSION['name_user'])) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active"> <a class="nav-link" href="panel-usuario"> Inicio <span class="sr-only">(current)</span></a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#"> Agregar amigo</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="AgregarAmigo"> Agregar amigo</a> </li>
                 <li class="nav-item"> <a class="nav-link" href="NuevaClase"> Crear clase</a> </li>
                 <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Noticias
                     </a>
