1afaf84bd21ebbe3fdb27b154f4d8adc8d74ba17
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2019-08-27 10:52:01 +0200

    mail

--- app/mail.php
+++ app/mail.php
@@ -1,8 +1,8 @@
 
 
 <?php
-    //ini_set( 'display_errors', 1 );
-    //error_reporting( E_ALL );
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
     $from = "contacto@myclass.es";
     $to = $_POST["email"];
     echo $to;
--- vistas/404.php
+++ vistas/404.php
@@ -124,7 +124,7 @@
     </div>
     <h2>Page not found</h2>
     <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-    <a href="../myclass">home page</a>
+    <a href="home">home page</a>
   </div>
 </div>
 
